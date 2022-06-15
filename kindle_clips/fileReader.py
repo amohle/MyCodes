289cc73ca70b6178b1278b58c7e0c53355780ded
amohle <am.computerscientist@gmail.com>
2022-06-15 13:44:52 +0530

    adding comments in the main code for kindle reader

--- kindle_clips/fileReader.py
+++ kindle_clips/fileReader.py
@@ -1,3 +1,6 @@
+#this code takes myclippings.txt file of the kindle (here renamed as kindle.txt) and removes
+#the book name as well as highlight information from the clippings.
+
 import re
 f=open("kindle.txt","r")
 f2=open("clippings.txt","w")
@@ -7,7 +10,7 @@ for line in f:
         status="out"
     if "Computer Networking" in line or status=="in":
         status="in"
-        res=re.sub(r'Computer Networking [\w|\W]*',"",line)
+        res=re.sub(r'Computer Networking [\w|\W]*',"",line) #Name of the book in kindle
         res2=re.sub(r'Your Highlight on Location [\w|\W]*AM|Your Highlight on Location [\w|\W]*PM',"",res)
         f2.write(res2)
 f.close()
